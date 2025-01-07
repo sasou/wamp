cd41b8bc2f435e11e365684ef8d6c806f08fbd29
sasou2008 <zaipd@qq.com>
2025-01-07 08:35:10 +0800

    update;

--- wampServer-php8.1_x64_nts/www/memcached.php
+++ wampServer-php8.1_x64_nts/www/memcached.php
@@ -1,16 +1,16 @@
 <?php 
 header("Content-type: text/html; charset=UTF-8");
-$memcache = new Memcache; 
-$memcache->connect('localhost', 11211) or die ("Could not connect"); 
+$memcache = new Memcached; 
+$memcache->addServer('localhost', 11211) or die ("Could not connect"); 
 
 $version = $memcache->getVersion(); 
-echo "Memcache服务器版本: ".$version."<br/>\n"; 
+echo "Memcache服务器版本: ".print_r($version)."<br/>\n"; 
 
 $tmp_object = new stdClass; 
 $tmp_object->str_attr = '缓存测试文本'; 
 $tmp_object->int_attr = 123; 
 
-$memcache->set('key', $tmp_object, false, 10) or die ("Failed to save data at the server"); 
+$memcache->set('key', $tmp_object, 10) or die ("Failed to save data at the server"); 
 echo "存入缓存数据 (设置10秒过期)<br/>\n"; 
 
 $get_result = $memcache->get('key'); 
